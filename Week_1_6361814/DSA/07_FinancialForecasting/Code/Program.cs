2de9b01120b85d772ae66b0e60d8f92d9803f5b9
JaahanviSingh19 <130581106+sjaahanvi@users.noreply.github.com>
2025-06-21 17:16:40 +0530

    Add files via upload

---
+++ Week_1_6361814/DSA/07_FinancialForecasting/Code/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Forecast
+{
+    public static double CalculateFutureValue(double value, double rate, int years)
+    {
+        if (years == 0)
+            return value;
+
+        return (1 + rate) * CalculateFutureValue(value, rate, years - 1);
+    }
+
+    public static double CalculateWithMemo(double value, double rate, int years, Dictionary<int, double> memo)
+    {
+        if (years == 0) return value;
+
+        if (memo.ContainsKey(years))
+            return memo[years];
+
+        double result = (1 + rate) * CalculateWithMemo(value, rate, years - 1, memo);
+        memo[years] = result;
+        return result;
+    }
+
+    static void Main()
+    {
+        double value = 1000;
+        double rate = 0.1;
+        int years = 5;
+
+        Console.WriteLine($"Future Value (Recursion): {CalculateFutureValue(value, rate, years):C}");
+
+        var memo = new Dictionary<int, double>();
+        Console.WriteLine($"Future Value (Memoized): {CalculateWithMemo(value, rate, years, memo):C}");
+    }
+}
