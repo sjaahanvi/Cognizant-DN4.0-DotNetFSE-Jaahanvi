55a7646177bd926ec436dc9968aa179ff300f40e
JaahanviSingh19 <130581106+sjaahanvi@users.noreply.github.com>
2025-07-05 20:07:46 +0530

    Add files via upload

---
+++ Week_3_6361814/RetailInventory/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace RetailInventory.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
