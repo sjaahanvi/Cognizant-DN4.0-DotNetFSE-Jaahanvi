cd78aece614294a08fbcb0803e47eb4bb65a7cd9
JaahanviSingh19 <130581106+sjaahanvi@users.noreply.github.com>
2025-07-20 23:00:12 +0530

    Add files via upload

---
+++ Week_5_6361814/Task 2/models/User.cs
@@ -0,0 +1,11 @@
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+public class LoginModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
