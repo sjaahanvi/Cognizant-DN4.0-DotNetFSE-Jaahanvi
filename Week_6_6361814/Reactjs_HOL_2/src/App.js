e859a9e11e369bf2a7a90a450404a1d3ae5c10ab
JaahanviSingh19 <130581106+sjaahanvi@users.noreply.github.com>
2025-07-27 20:33:58 +0530

    Add files via upload

---
+++ Week_6_6361814/Reactjs_HOL_2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './Components/Home';
+import About from './about';
+import Contact from './contact';
+
+function App() {
+  return (
+    <div>
+      <h1>Student Management Portal</h1>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
